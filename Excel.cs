feec239e9dcd100afaa7b1e6b416c856bfea4794
Haitham T. Abdulkadier (HalabjaIT) <hiethem.hiethem@yahoo.com>
2016-11-24 10:40:42 +0300

    First

---
+++ Excel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+namespace HOffice
+{
+    public class Excel
+    {
+        Application app = new Application();
+        Workbook book;
+        Worksheet sheet;
+        string path;
+        public Excel(string path)
+        {
+            this.path = path;
+        }
+        public bool Open()
+        {
+            book = app.Workbooks.Open(path);
+            this.sheet = (Worksheet)book.Sheets[1];
+            return true;
+        }
+        public bool Quit()
+        {
+            app.Quit();
+            return true;
+        }
+        public bool Write(string value, int column = 1,int row = 1, int sheet = 1)
+        {
+            this.sheet = (Worksheet)book.Sheets[sheet];
+            this.sheet.Cells[row, column].Value = value;
+            book.Save();
+            return true;
+        }
+        public int lastRow()
+        {
+            return this.sheet.Cells.SpecialCells(XlCellType.xlCellTypeLastCell).Row;
+        }
+    }
+}
